bd7db66ad4c1023538c3b3b93d04e32c8820de18
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-03-09 10:50:25 +0800

    feat: Initialize message data

--- app.js
+++ app.js
@@ -11,3 +11,17 @@ app.listen(process.env.PORT, (error) => {
 		console.error(error);
 	}
 });
+
+// Message data
+const messages = [
+	{
+		text: 'Hi there!',
+		user: 'Armando',
+		added: new Date(),
+	},
+	{
+		text: 'Hell World!',
+		user: 'Charles',
+		added: new Date(),
+	},
+];
